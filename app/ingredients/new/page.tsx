25e26047e6a36450a1aea5a139e6526e769b483d
John Prada <jhonprada@gmail.com>
2023-07-10 18:46:37 -0500

    initial commit

---
+++ app/ingredients/new/page.tsx
@@ -0,0 +1,14 @@
+import PageTitle from '@/components/PageTitle';
+import { buttonVariants  } from "@/components/ui/button"
+import Link from "next/link";
+
+export default function NewIngredientPage() {
+  return (
+    <main>
+     <PageTitle title='Crear Ingrediente'/>
+     <section className="my-4">
+
+     </section>
+    </main>
+  );
+}
