36947eadd8fbbdba6c5143fb210f1fd626072b7e
John Prada <jhonprada@gmail.com>
2023-07-12 13:13:42 -0500

    added form fist input and prisma

--- app/ingredients/new/page.tsx
+++ app/ingredients/new/page.tsx
@@ -1,13 +1,22 @@
 import PageTitle from '@/components/PageTitle';
-import { buttonVariants  } from "@/components/ui/button"
-import Link from "next/link";
+import { Label } from "@/components/ui/label"
+import { Input } from "@/components/ui/input"
+import { Ingredient } from '@prisma/client';
+import z from 'zod';
+
+
 
 export default function NewIngredientPage() {
   return (
     <main>
      <PageTitle title='Crear Ingrediente'/>
      <section className="my-4">
-
+    <form >
+    <div >
+      <Label htmlFor="name">Nombre del ingrediente</Label>
+      <Input id="name" type="text" placeholder='Ingresa el nombre de tu ingrediente' />
+    </div>
+    </form>
      </section>
     </main>
   );
