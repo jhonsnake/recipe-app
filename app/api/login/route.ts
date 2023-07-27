1b4d69ddf1465bb5f73de30e5879bb9d740def29
John Prada <jhonprada@gmail.com>
2023-07-27 12:34:23 -0500

    added create users from api

---
+++ app/api/login/route.ts
@@ -0,0 +1,39 @@
+import { prisma } from "@/db";
+import * as bcrypt from "bcrypt";
+import { NextResponse } from "next/server";
+
+interface RequestBody {
+  username: string;
+  password: string;
+}
+
+export default async function Post(request: Request) {
+  // Parse the request body
+  const body: RequestBody = await request.json();
+
+  // Extract the username and password values
+  const { username, password } = body;
+
+  // Query the database for the user
+  const user = await prisma.user.findUnique({
+    where: {
+      email: username,
+    },
+  });
+
+  // Check whether the user exists and the password is correct
+  if (user && (await bcrypt.compare(password, user.password))) {
+    // If the username and password match, return a success message and the user
+    const { password, ...userWithoutPass } = user;
+    return NextResponse.json(
+      { message: "Success", user: userWithoutPass },
+      { status: 200, statusText: "OK" }
+    );
+  }
+
+  // If the username and password don't match, return a failure message
+  return NextResponse.json(
+    { message: "Error", error: "Invalid credentials" },
+    { status: 401, statusText: "Unauthorized" }
+  );
+}
---
+++ app/api/user/route.ts
@@ -0,0 +1,26 @@
+import { prisma } from "@/db";
+import { User } from "@prisma/client";
+import { NextResponse } from "next/server";
+import * as bcrypt from "bcrypt";
+
+type RequestBody = Omit<User, "createdAt" | "updatedAt" | "id">;
+
+export async function POST(request: Request) {
+  const body: RequestBody = await request.json();
+
+  const { email, name, password } = body;
+
+  const user = await prisma.user.create({
+    data: {
+      email,
+      name,
+      password: await bcrypt.hash(password, 10),
+    },
+  });
+  const { password: _, ...userWithoutPass } = user;
+
+  return NextResponse.json(
+    { message: "Success", user: userWithoutPass },
+    { status: 201, statusText: "Created" }
+  );
+}
